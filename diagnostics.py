703b51a0c6b82577ba781096f696884ccebdbb51
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-03-05 07:23:29 +0800

    Update diagnostics.py

--- diagnostics.py
+++ diagnostics.py
@@ -1,31 +1,30 @@
 import pandas as pd
-from datetime import datetime
-import csv
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import numpy as np
+
 df = pd.read_csv('df/data_log.csv')
-plt.figure(figsize=(40,15))
+plt.figure(figsize=(40, 15))
 
 dfb = df[['motion', 'motion_sma']]
-df_scaled_sma = (dfb['motion_sma']-dfb['motion'].min())/(dfb['motion'].max()-dfb['motion'].min())
-df_scaled_motion = (dfb['motion']-dfb['motion'].min())/(dfb['motion'].max()-dfb['motion'].min())
-df_scaled = pd.concat((df['ts'], df_scaled_motion, df_scaled_sma),axis=1)
+
+df_scaled_sma = (dfb['motion_sma'] - dfb['motion'].min()) / (dfb['motion'].max() - dfb['motion'].min())
+df_scaled_motion = (dfb['motion'] - dfb['motion'].min()) / (dfb['motion'].max() - dfb['motion'].min())
+df_scaled = pd.concat((df['ts'], df_scaled_motion, df_scaled_sma), axis=1)
+
 print("Summary")
 print(df.describe())
 print(df_scaled.describe())
 print(df_scaled.var())
-plt.scatter(x=df_scaled.iloc[:,0], y=df_scaled.iloc[:,1], s=2, marker='+')
-plt.scatter(x=df_scaled.iloc[:,0], y=df_scaled.iloc[:,2], s=3, marker='^')
-plt.axhline(y=350000/df['motion'].max(), color='orange', linestyle='--', linewidth=1)
-plt.yticks(np.arange(0,1,0.01))
-plt.xticks(np.arange(df_scaled.iloc[:,0].min(), df_scaled.iloc[:,0].max(), 10), rotation=90)
+plt.scatter(x=df_scaled.iloc[:, 0], y=df_scaled.iloc[:, 1], s=2, marker='+')
+plt.scatter(x=df_scaled.iloc[:, 0], y=df_scaled.iloc[:, 2], s=3, marker='^')
+plt.axhline(y=350000 / df['motion'].max(), color='orange', linestyle='--', linewidth=1)
+plt.yticks(np.arange(0, 1, 0.01))
+plt.xticks(np.arange(df_scaled.iloc[:, 0].min(), df_scaled.iloc[:, 0].max(), 10), rotation=90)
 
 plt.grid(True, color='gray', linestyle='-', linewidth=0.5, alpha=0.5)
 plt.savefig("threshold_plot.png")
 
-
 # 0.07 -> 1.529747e7
 # 0.08 -
 
-#df['Date'] = df['Date'].map(lambda x: datetime.strptime(str(x), '%Y/%m/%d %H:%M:%S.%f'))
+# df['Date'] = df['Date'].map(lambda x: datetime.strptime(str(x), '%Y/%m/%d %H:%M:%S.%f'))
